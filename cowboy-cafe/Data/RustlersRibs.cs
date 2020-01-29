ce2c2220236b141bd000fbaeff12fa82fe17ed3d
Benjamin Graham <grahamfamily15@hotmail.com>
2020-01-29 17:03:57 -0600

    Checkpoint 1

---
+++ cowboy-cafe/Data/RustlersRibs.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// A class representing the RustlersRibs entree
+    /// </summary>
+    public class RustlersRibs
+    {
+
+        /// <summary>
+        /// The price of the ribs
+        /// </summary>
+        public double Price
+        {
+            get
+            {
+                return 7.50;
+            }
+        }
+
+        /// <summary>
+        /// The calories of the ribs
+        /// </summary>
+        public uint Calories
+        {
+            get
+            {
+                return 894;
+            }
+        }
+        
+        /// <summary>
+        /// returns empty list since no special instructions
+        /// </summary>
+        public List<string> SpecialInstructions
+        {
+            get
+            {
+
+                var instructions = new List<string>();
+
+                return instructions;
+            }
+        }
+
+    }
+}
+
