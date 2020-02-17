815916257b7b51dacc4ecfc4dab7409f8fc3c5ce
Benjamin Graham <grahamfamily15@hotmail.com>
2020-02-16 20:51:51 -0600

    Added UML Diagram

--- Data/CowpokeChili.cs
+++ Data/CowpokeChili.cs
@@ -43,14 +43,14 @@ namespace CowboyCafe.Data
             set { greenOnions = value; }
         }
 
-        private bool tortillaStrips = true;
+        private bool tortillaChips = true;
         /// <summary>
         /// If the chili is topped with tortilla strips
         /// </summary>
-        public bool TortillaStrips
-        {
-            get { return tortillaStrips; }
-            set { tortillaStrips = value; }
+        public bool TortillaChips 
+        { 
+            get { return tortillaChips; }
+            set { tortillaChips = value; }
         }
 
         /// <summary>
@@ -87,7 +87,7 @@ namespace CowboyCafe.Data
                 if (!cheese) instructions.Add("hold cheese");
                 if (!sourCream) instructions.Add("hold sour cream");
                 if (!greenOnions) instructions.Add("hold green onions");
-                if (!tortillaStrips) instructions.Add("hold tortilla strips");
+                if (!tortillaChips) instructions.Add("hold tortilla strips");
 
                 return instructions;
             }
