942341a1f74d5cf6233b7deeed15f53cf98b4cab
Benjamin Graham <grahamfamily15@hotmail.com>
2020-02-19 16:11:52 -0600

    nearly finished

--- PointOfSale/OrderControl.xaml.cs
+++ PointOfSale/OrderControl.xaml.cs
@@ -10,7 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using CowboyCafe.Data;
 namespace PointOfSale
 {
     /// <summary>
@@ -21,7 +21,90 @@ namespace PointOfSale
         public OrderControl()
         {
             InitializeComponent();
-            AddAngryChicken.Height = 40;
+
+            
+        }
+
+        private void AddAngryChicken_Click(object sender, RoutedEventArgs e)
+        {
+            OrderListView.Items.Add(new AngryChicken());
+        }
+
+        private void AddCowpokeChili_Click(object sender, RoutedEventArgs e)
+        {
+            OrderListView.Items.Add(new CowpokeChili());
+        }
+
+        private void AddRustlersRibs_Click(object sender, RoutedEventArgs e)
+        {
+            OrderListView.Items.Add(new RustlersRibs());
+
+        }
+
+        private void PecosPulledPork_Click(object sender, RoutedEventArgs e)
+        {
+            OrderListView.Items.Add(new PecosPulledPork());
+
+        }
+
+        private void AddTrailBurger_Click(object sender, RoutedEventArgs e)
+        {
+            OrderListView.Items.Add(new TrailBurger());
+
+        }
+
+        private void AddDakotaDoubleBurger_Click(object sender, RoutedEventArgs e)
+        {
+            OrderListView.Items.Add(new DakotaDoubleBurger());
+
+        }
+
+        private void AddTexasTripleBurger_Click(object sender, RoutedEventArgs e)
+        {
+            OrderListView.Items.Add(new TexasTripleBurger());
+
+        }
+
+        private void AddPanDeCampo_Click(object sender, RoutedEventArgs e)
+        {
+            OrderListView.Items.Add(new PanDeCampo());
+
+        }
+
+        private void AddCornDodgers_Click(object sender, RoutedEventArgs e)
+        {
+            OrderListView.Items.Add(new CornDodgers());
+
+        }
+
+        private void AddChiliCheeseFries_Click(object sender, RoutedEventArgs e)
+        {
+            OrderListView.Items.Add(new ChiliCheeseFries());
+
+        }
+
+        private void AddJerkedSoda_Click(object sender, RoutedEventArgs e)
+        {
+            OrderListView.Items.Add(new JerkedSoda());
+
+        }
+
+        private void AddTexasTea_Click(object sender, RoutedEventArgs e)
+        {
+            OrderListView.Items.Add(new TexasTea());
+
+        }
+
+        private void AddCowboyCoffee_Click(object sender, RoutedEventArgs e)
+        {
+            OrderListView.Items.Add(new CowboyCoffee());
+
+        }
+
+        private void AddWater_Click(object sender, RoutedEventArgs e)
+        {
+            OrderListView.Items.Add(new Water());
+
         }
     }
 }
